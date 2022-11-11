03357b53f214ba006680e38420a5b3237aede625
Fathan Rangga <muhammad.jaya@binus.ac.id>
2022-11-11 19:24:25 +0700

    Fist Upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("button-toggle").addEventListener("click", () => {
+    document.getElementById("sidebar").classList.toggle("close");
+    document.getElementById("main-content").classList.toggle("open");
+});
